c6f612cff5b560acb07a5d7f1c8e4ba33b95f739
Keks <keks@htmlacademy.ru>
2022-06-07 16:56:22 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,23 @@
+function getRandomInt(min, max) {
+  if (max <= min) {
+    return console.log('Измените значение max. Оно должно быть больше значения min');
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
+console.log(getRandomInt(0, 9));
+
+
+function getLengthComment(line, length) {
+  const lineLength = line.length;
+  if (lineLength> length) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(getLengthComment(5, 10));
